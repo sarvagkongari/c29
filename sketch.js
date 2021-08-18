3d1f7b4748819f2119712cdec20c0077de68c1cf
sarvagkongari <86407771+sarvagkongari@users.noreply.github.com>
2021-08-18 14:19:11 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,42 @@
+const Engine = Matter.Engine;
+const Render = Matter.Render;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+const Body = Matter.Body;
+const Composites = Matter.Composites;
+const Composite = Matter.Composite;
+
+var ground,leftWall,rightWall,bridge,jointLink,jointPoint
+
+var stone = [];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  engine = Engine.create();
+  world = engine.world;
+  frameRate(80);
+  ground= new Base(0,height-10,width*2,20,"#795548",true)
+  leftWall=new Base(300,height/2+50,600,100,"#8d6e63",true)
+  rightWall=new Base(width-300,height/2+50,600,100,"#8d6e63",true)
+  bridge=new bridge(15,{x:width/2-400,y:height/2})
+  jointPoint=new Base(-600,height/2+10,40,20,"8d6e63",true)
+}
+
+function preLoad(){
+  
+}
+
+
+function draw() {
+  background(51);
+  Engine.update(engine);
+
+}
+
+for (var i=0; i <=8; i++ ){
+  var x= random(width /2-200,width/2+300);
+  var y= random(-10,140);
+  var stone=new stone(x,y,80,80);
+  stones.push(stone)
+}
